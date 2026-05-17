8f81a68321a0bad07aaeb514eece66cd908ee812
Alberer Christoph <Christoph.Alberer@lisec.com>
2026-05-17 18:36:12 +0200

    Lists and Dictionaries in Python

--- python_work/favorite_languages.py
+++ python_work/favorite_languages.py
@@ -6,3 +6,15 @@ favorite_languages = {
 }
 language = favorite_languages['sarah'].title()
 print("Sarah's favorite language is " + favorite_languages['sarah'].title() + ".")
+
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+#Schlüssel sortiert durchlaufen
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+
+#Werte durchlaufen
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
